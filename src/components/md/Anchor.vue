9eddcfecde92ad1286c411bed79386cb911f261e
Xecades <i@xecades.xyz>
2025-02-05 13:34:33 +0800

    style: change markdown style

--- src/components/md/Anchor.vue
+++ src/components/md/Anchor.vue
@@ -18,10 +18,16 @@ const is_internal: boolean = !props.href.startsWith("http");
 a
     scheme(--color, $theme-color, lighten($theme-color, 10%));
     scheme(--hover-color, darken($theme-color, 45%), darken($theme-color, 20%));
+    scheme(--hover-underline-color, darken($theme-color, 30%), darken($theme-color, 20%));
 
     color: var(--color);
     transition: color 0.07s;
+    text-decoration: underline;
+    text-underline-offset: 3px;
+    text-decoration-style: dashed;
 
     &:hover
         color: var(--hover-color);
+        text-decoration-style: solid;
+        text-decoration-color: var(--hover-underline-color);
 </style>
